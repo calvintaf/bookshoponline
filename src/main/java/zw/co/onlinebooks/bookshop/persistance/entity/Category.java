8c535d331a181ad3e26e2f30cc045b0f1186f170
calvin.mapfumo <calvin.mapfumo@wonderlabz.com>
2023-04-28 14:50:18 +0200

    Added Category

--- src/main/java/zw/co/onlinebooks/bookshop/persistance/entity/Category.java
+++ src/main/java/zw/co/onlinebooks/bookshop/persistance/entity/Category.java
@@ -1,2 +1,22 @@
-package zw.co.onlinebooks.bookshop.persistance.entity;public class Category {
+package zw.co.onlinebooks.bookshop.persistance.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "category")
+@Data
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long categoryId;
+
+    @Column(name = "title")
+    private String title;
 }
