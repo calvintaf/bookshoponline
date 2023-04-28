3df760d2636964e10f5e438128f777acc613d6e2
calvin.mapfumo <calvin.mapfumo@wonderlabz.com>
2023-04-28 14:52:10 +0200

    Added BookDto

--- src/main/java/zw/co/onlinebooks/bookshop/model/BookDto.java
+++ src/main/java/zw/co/onlinebooks/bookshop/model/BookDto.java
@@ -1,2 +1,21 @@
-package zw.co.onlinebooks.bookshop.model;public class Book {
+package zw.co.onlinebooks.bookshop.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BookDto {
+    private Long bookId;
+    private String description;
+    private String title;
+    private BigDecimal price;
+    private CategoryDto categoryDto;
 }
+
+
