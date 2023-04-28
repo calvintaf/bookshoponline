9e3c5a941833c08e42304ca824440288cda4661c
calvin.mapfumo <calvin.mapfumo@wonderlabz.com>
2023-04-28 15:02:07 +0200

    Refactored CategoryService

--- src/main/java/zw/co/onlinebooks/bookshop/service/CategoryService.java
+++ src/main/java/zw/co/onlinebooks/bookshop/service/CategoryService.java
@@ -3,6 +3,10 @@ package zw.co.onlinebooks.bookshop.service;
 import zw.co.onlinebooks.bookshop.model.CategoryDto;
 import zw.co.onlinebooks.bookshop.persistance.entity.Category;
 
+import java.util.List;
+
 public interface CategoryService {
     Category createCategory(CategoryDto categoryDto);
+    Category updateCategory(CategoryDto categoryDto);
+    List<Category> getAllCategories();
 }
