198d45ff03cdbb626d3662391590278d7892789a
calvin.mapfumo <calvin.mapfumo@wonderlabz.com>
2023-04-28 14:35:29 +0200

    Added BookService

--- src/main/java/zw/co/onlinebooks/bookshop/service/BookService.java
+++ src/main/java/zw/co/onlinebooks/bookshop/service/BookService.java
@@ -1,2 +1,11 @@
-package zw.co.onlinebooks.bookshop.service.impl;public interface BookService {
+package zw.co.onlinebooks.bookshop.service;
+
+import zw.co.onlinebooks.bookshop.model.BookDto;
+import zw.co.onlinebooks.bookshop.persistance.entity.Book;
+
+import java.util.List;
+
+public interface BookService {
+    Book createBook(BookDto bookDto);
+    List<Book> getAllBooks();
 }
