ea1a0d3ecb29d91bd78b757f6006dc383ddefa9d
calvin.mapfumo <calvin.mapfumo@wonderlabz.com>
2023-04-28 13:20:44 +0200

    Initial commit

---
+++ src/test/java/zw/co/onlinebooks/bookshop/BookshopApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.onlinebooks.bookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
