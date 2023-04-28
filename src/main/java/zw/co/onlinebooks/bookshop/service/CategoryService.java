d2c1ed150425de661cb14a527a1914f58fb03101
calvin.mapfumo <calvin.mapfumo@wonderlabz.com>
2023-04-28 14:49:27 +0200

    Added CategoryService

--- src/main/java/zw/co/onlinebooks/bookshop/service/CategoryService.java
+++ src/main/java/zw/co/onlinebooks/bookshop/service/CategoryService.java
@@ -1,2 +1,8 @@
-package zw.co.onlinebooks.bookshop.service;public interface CategoryService {
+package zw.co.onlinebooks.bookshop.service;
+
+import zw.co.onlinebooks.bookshop.model.CategoryDto;
+import zw.co.onlinebooks.bookshop.persistance.entity.Category;
+
+public interface CategoryService {
+    Category createCategory(CategoryDto categoryDto);
 }
