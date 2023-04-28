44f361a6625df37e1754fed115ad7d25aed4db7b
calvin.mapfumo <calvin.mapfumo@wonderlabz.com>
2023-04-28 14:52:41 +0200

    Added CategoryDto

--- src/main/java/zw/co/onlinebooks/bookshop/model/CategoryDto.java
+++ src/main/java/zw/co/onlinebooks/bookshop/model/CategoryDto.java
@@ -1,2 +1,15 @@
-package zw.co.onlinebooks.bookshop.model;public class CategoryDto {
+package zw.co.onlinebooks.bookshop.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryDto {
+    private Long categoryId;
+    private String Title;
 }
