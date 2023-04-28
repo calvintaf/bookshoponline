5fc8852c8b6c76ecf9fb2aa5e021b351f80fa028
calvin.mapfumo <calvin.mapfumo@wonderlabz.com>
2023-04-28 14:48:23 +0200

    Added BookEntity

--- src/main/java/zw/co/onlinebooks/bookshop/persistance/entity/Book.java
+++ src/main/java/zw/co/onlinebooks/bookshop/persistance/entity/Book.java
@@ -1,2 +1,36 @@
-package zw.co.onlinebooks.bookshop.persistance.entity;public class Book {
+package zw.co.onlinebooks.bookshop.persistance.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "book")
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long bookId;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "is_sold")
+    private Boolean isSold = false;
 }
