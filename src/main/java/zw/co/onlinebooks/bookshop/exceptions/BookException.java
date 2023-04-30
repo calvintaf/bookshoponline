83c52a0f8dd24ea757af4f8a948aad8313a95a5f
calvin.mapfumo <calvin.mapfumo@wonderlabz.com>
2023-04-30 10:53:38 +0200

    Added Exception classes

---
+++ src/main/java/zw/co/onlinebooks/bookshop/exceptions/BookException.java
@@ -0,0 +1,7 @@
+package zw.co.onlinebooks.bookshop.exceptions;
+
+public class BookException extends Throwable {
+    public BookException(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/zw/co/onlinebooks/bookshop/exceptions/CategoryException.java
@@ -0,0 +1,7 @@
+package zw.co.onlinebooks.bookshop.exceptions;
+
+public class CategoryException extends RuntimeException{
+    public CategoryException(String s) {
+        super(s);
+    }
+}
