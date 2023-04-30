a170837d24ff4ccbd1709a00cc2f638124c02c70
calvin.mapfumo <calvin.mapfumo@wonderlabz.com>
2023-04-30 10:53:15 +0200

    Refactored BookController

---
+++ src/main/java/zw/co/onlinebooks/bookshop/controller/BookController.java
@@ -0,0 +1,57 @@
+package zw.co.onlinebooks.bookshop.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import zw.co.onlinebooks.bookshop.exceptions.BookException;
+import zw.co.onlinebooks.bookshop.model.BookDto;
+import zw.co.onlinebooks.bookshop.persistance.entity.Book;
+import zw.co.onlinebooks.bookshop.service.BookService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/book")
+public class BookController {
+
+    private final BookService bookService;
+
+    @PostMapping
+    public Book createBook(@RequestBody BookDto bookDto) {
+        return bookService.createBook(bookDto);
+    }
+
+    @GetMapping("/bookd")
+    public Book getBook(@PathVariable Long bookId) throws BookException {
+        return bookService.getBook(bookId);
+    }
+
+    @GetMapping("/all")
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/available")
+    public List<Book> getAvailableBooks() {
+        return bookService.getAvailableBooks();
+    }
+
+    @PutMapping("/{bookId}")
+    public Book updateBook(@PathVariable Long bookId, @RequestBody BookDto bookDto) throws BookException {
+        return bookService.updateBook(bookId, bookDto);
+    }
+
+    @DeleteMapping("/{bookId}")
+    public Book removeBook(@PathVariable Long bookId) throws BookException {
+        return bookService.remove(bookId);
+    }
+}
