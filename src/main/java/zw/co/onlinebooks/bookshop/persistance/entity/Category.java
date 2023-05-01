0968ef893d52868dee0570b0e907bc068e49d75e
calvin.mapfumo <calvin.mapfumo@wonderlabz.com>
2023-05-01 18:11:20 +0200

    Code clean up

--- src/main/java/zw/co/onlinebooks/bookshop/persistance/entity/Category.java
+++ src/main/java/zw/co/onlinebooks/bookshop/persistance/entity/Category.java
@@ -7,20 +7,18 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
-import java.io.Serializable;
 
 
 
 @Entity
 @Table(name = "category")
 @Data
-public class Category implements Serializable{
+public class Category{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false, updatable = false)
     private Long id;
 
-    @Column(name = "title")
     private String title;
 
 }
