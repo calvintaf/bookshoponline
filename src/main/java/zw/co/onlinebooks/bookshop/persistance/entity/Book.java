c3faeea4899aadf14e380ff55486ffb53bb8edbf
calvin.mapfumo <calvin.mapfumo@wonderlabz.com>
2023-05-01 18:10:19 +0200

    Code clean up

--- src/main/java/zw/co/onlinebooks/bookshop/persistance/entity/Book.java
+++ src/main/java/zw/co/onlinebooks/bookshop/persistance/entity/Book.java
@@ -1,7 +1,5 @@
 package zw.co.onlinebooks.bookshop.persistance.entity;
 
-import jakarta.persistence.Cacheable;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -10,37 +8,30 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
-import java.io.Serializable;
 import java.math.BigDecimal;
 
 
 @Entity
 @Table(name = "book")
 @Data
-public class Book extends Category implements Serializable {
+public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "book_id", nullable = false, updatable = false)
     private Long id;
 
-    @Column(name = "title")
     private String title;
 
-    @Column(name = "description")
     private String description;
 
-    @Column(name = "price")
     private BigDecimal price;
 
-    @Column(name = "is_available")
-    private Boolean isAvailable = true;
+    private Boolean is_Available = true;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     private Category category;
-
 
 }
